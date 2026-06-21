f9ed0b48d81f88022e82837a916969718b368e81
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-06-21 18:10:56 +0530

    added endpoints

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const {userRegisterController} = require("../controllers/auth.controller.js")
+
+const router = express.Router();
+
+router.post("/register",userRegisterController)
+
+
+module.exports = router
